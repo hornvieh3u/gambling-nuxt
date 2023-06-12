d8fc11ecd81164a839560c84477403652dc119f4
jacob0x2 <fantaciveEvolution1248@gmail.com>
2023-06-11 23:11:24 -0700

    catch click_ic and promo from url

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,24 @@
 <script setup lang="ts">
 import { useStore } from 'vuex';
-import {ref} from 'vue';
+import { ref , onBeforeMount } from 'vue';
 import Conclusion from '~~/components/landingPage/Conclusion.vue';
 import GameList from '~~/components/landingPage/GameList.vue';
 import ProviderList from '~~/components/landingPage/ProviderList.vue';
 import Activity from '~~/components/landingPage/Activity.vue';
-const store = useStore();
+import {useRoute} from 'vue-router';
+import Cookies from 'js-cookie'
 
+const store = useStore();
+const route = useRoute();
+onBeforeMount(()=>{
+    console.log(route.query.click_id);
+    const click_id = route.query.click_id;
+    const promo = route.query.promo;
+    if(click_id)
+        Cookies.set('click_id', click_id.toString());
+    if(promo)
+        Cookies.set('promo', promo.toString());  
+});
 </script>
 <template>
     <q-page>
